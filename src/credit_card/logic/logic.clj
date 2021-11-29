1405245cd4cd170ba156adfa229d3d78e9dc8c62
JessicaCavalcante <jessicamcavalcante07@gmail.com>
2021-11-29 16:18:38 -0300

    altered logic file

--- src/credit_card/logic/logic.clj
+++ src/credit_card/logic/logic.clj
@@ -1,96 +1,18 @@
 (ns credit_card.logic.logic
   (:require [credit_card.db.order :as db.order]))
 
-
-;(println (db.order/all-orders))
-
-(println (group-by :card_id (db.order/all-orders)))
-
-(->> (db.order/all-orders)
-     (group-by :card_id)
-     vals
-     (map count)
-     println)
-;
-;(defn total-card
-;  [[_ card-id]]
-;  (count card-id))
-;
-;(->> (db.order/all-orders)
-;     (group-by :id)
-;     (map total-card)
-;     println)
-;
-;(println  "teste" (map count (vals (group-by :card_id (db.order/all-orders)))))
-
-;(defn total-do-item
-;  [[_ detalhes]]
-;  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
-;
-;(defn total-do-pedido
-;  [pedido]
-;  (reduce + (map total-do-item pedido)))
-;
-;(defn total-dos-pedidos
-;  [pedidos]
-;  (->> pedidos
-;       (map :itens)
-;       (map total-do-pedido)
-;       (reduce +)))
-;
-;(defn quantia-de-pedidos-e-gasto-total-por-usuario
-;  [[usuario pedidos]]
-;  {:usuario-id usuario
-;   :total-de-pedidos (count pedidos)
-;   :preco-total (total-dos-pedidos pedidos)})
-;
-;(defn resumo-por-usuario [pedidos]
-;  (->> pedidos
-;       (group-by :usuario)
-;       (map quantia-de-pedidos-e-gasto-total-por-usuario)))
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;(defn minha-funcao-de-agrupamento
-;  [elemento]
-;  (println "elemento" elemento)
-;  (:card_id elemento)
-;  )
-
-;(println (group-by minha-funcao-de-agrupamento (db.order/all-orders)))
-
-(println "PEDIDOS")
-
-;(defn total-purchase-card
-;  [[card-id orders]]
-;  [card-id (count orders)])
-;
-;(->> (db.order/all-orders)
-;     (group-by :card_id)
-;     (map total-purchase-card)
-;     println)
-;
-;(defn total-purchase-card
-;  [[card-id orders]]
-;  {
-;   :card-id card-id
-;   :total-purchase-card (count orders)})
-;
-;(->> (db.order/all-orders)
-;     (group-by :card_id)
-;     (map total-purchase-card)
-;     println)
-
-;(defn price-order
-;  [[chave valor]]
-;  (:price valor))
+(defn price-order
+  [[chave valor]]
+  (:price valor))
 
 (defn sum-prices
   [pedido]
   (->> pedido
-       ;(map :info)
+       (map :info)
+       :price
+       (reduce +)
        ;(map price-order)
-       (map :price)
+       ;(map :price)
        println))
 
 (defn total-purchase-card
@@ -104,6 +26,12 @@
      (group-by :card_id)
      (map total-purchase-card)
      println)
+
+(->> (db.order/all-orders)
+     (group-by :card_id)
+     vals
+     println)
+
 
 
 
