fa6540e40f4a1442917c819cb747e34dde95c8d3
JessicaCavalcante <jessicamcavalcante07@gmail.com>
2021-12-03 16:39:05 -0300

    refactoring date in orders

--- src/credit_card/db/order.clj
+++ src/credit_card/db/order.clj
@@ -1,116 +1,68 @@
 (ns credit_card.db.order)
+(require '[clj-time.core :as t])
+(require '[clj-time.format :as f])
 
-;(def order1 {:id 1
-;             :info {:price 20
-;                    :establishment "Ifood"
-;                    :date "25/11/2021"
-;                    :category "food"
-;                    :card_id 1}})
-;
-;(def order2 {:id 2
-;             :info {:price 80
-;                    :establishment "Uber"
-;                    :date "24/11/2021"
-;                    :category "transport"
-;                    :card_id 2}})
-;
-;(def order3 {:id 3
-;             :info {:price 200
-;                    :establishment "Marisa"
-;                    :date "20/11/2021"
-;                    :category "clothes"
-;                    :card_id 3}})
-;
-;(def order4 {:id 4
-;             :info {:price 150
-;                    :establishment "Dafiti"
-;                    :date "22/11/2021"
-;                    :category "clothes"
-;                    :card_id 4}})
-;
-;(def order5 {:id 5
-;             :info {:price 40
-;                    :establishment "99"
-;                    :date "20/11/2021"
-;                    :category "transport"
-;                    :card_id 1}})
-;
-;(def order6 {:id 6
-;             :info {:price 20
-;                    :establishment "Uber"
-;                    :date "10/11/2021"
-;                    :category "transport"
-;                    :card_id 2}})
-;
-;(def order7 {:id 7
-;             :info {:price 20
-;                    :establishment "UberEats"
-;                    :date "10/11/2021"
-;                    :category "food"
-;                    :card_id 3}})
-;
-;(def order8 {:id 8
-;             :info {:price 20
-;                    :establishment "CEA"
-;                    :date "10/11/2021"
-;                    :category "clothes"
-;                    :card_id 4}})
+(def custom-formatter (f/formatter "YYYY/MM/dd"))
+
 
 (def order1 {:card-id 1
              :info {:price 20
                     :establishment "Ifood"
-                    :date "25/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 3))
                     :category "food"
                     }})
 
 (def order2 {:card-id 2
              :info {:price 80
                     :establishment "Uber"
-                    :date "24/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 24))
                     :category "transport"
                     }})
 
 (def order3 {:card-id 3
              :info {:price 200
                     :establishment "Marisa"
-                    :date "20/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 23))
                     :category "clothes"
                     }})
 
 (def order4 {:card-id 4
              :info {:price 150
                     :establishment "Dafiti"
-                    :date "22/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 22))
                     :category "clothes"
                     }})
 
 (def order5 {:card-id 1
              :info {:price 40
                     :establishment "99"
-                    :date "20/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 20))
                     :category "transport"
                     }})
 
 (def order6 {:card-id 2
              :info {:price 20
                     :establishment "Uber"
-                    :date "10/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 10))
                     :category "transport"
                     }})
 
 (def order7 {:card-id 3
              :info {:price 20
                     :establishment "UberEats"
-                    :date "10/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 10))
                     :category "food"
                     }})
 
 (def order8 {:card-id 4
              :info {:price 20
                     :establishment "CEA"
-                    :date "10/11/2021"
+                    :date (f/unparse custom-formatter (t/date-time 2021 11 10))
                     :category "clothes"
                     }})
 
 (defn all-orders []
   [order1, order2, order3, order4, order5, order6, order7, order8])
+
+;(println (:date (:info order1)))
+;(println order1)
