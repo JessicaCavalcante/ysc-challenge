8deeb4787dc3dd42113de2eaecdba3811389901b
JessicaCavalcante <jessicamcavalcante07@gmail.com>
2021-12-05 15:07:44 -0300

    tests

---
+++ test/credit_card/logic_test.clj
@@ -0,0 +1,22 @@
+(ns credit_card.logic-test
+  (:use clojure.pprint)
+  (:require [clojure.test :refer :all]
+            [credit_card.logic-test :refer :all]
+            [credit_card.logic.logic :refer :all]
+            [schema.core :as s]))
+
+(s/set-fn-validation! true)
+
+(deftest sum-total-test
+
+  (testing "Testing sum function"
+    (let [prices [{:price 10}, {:price 15}]]
+      (is (= 25
+             (sum-total prices))))))
+
+(deftest total-expenses-by-category-test
+
+  (testing "testing total expenses byn category"
+    (let [expenses ["food" [{:price 20, :category "food"}, {:price 40, :category "food"}]]]
+    (is (= {:category "food", :total-expenses 60}
+           (total-expenses-by-category expenses))))))
